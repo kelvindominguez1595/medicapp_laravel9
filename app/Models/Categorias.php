9f7a97fccca82027da6077bd4f8aff756dad380e
kelvindominguez1595 <44550467+kelvindominguez1595@users.noreply.github.com>
2023-04-29 12:35:15 -0600

    solvente problema de registrar

--- app/Models/Categorias.php
+++ app/Models/Categorias.php
@@ -1,16 +1,14 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
 class Categorias extends Model
 {
- use HasFactory;
+    use HasFactory;
 
- protected $table = "categorias";
- protected $primaryKey = "id";
-
- public $timestamps=true;
+    protected $table = 'categorias';
+    protected $fillable = ['categoria'];
 }
--- resources/views/generos/index.blade.php
+++ resources/views/generos/index.blade.php
@@ -1,48 +1,49 @@
 @extends('layouts.template')
 @section('content')
+    <div class="card">
+        <h5 class="card-header">Géneros</h5>
+        <div class="card-body">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col col-sm-12">
+                        @if ($mensaje = Session::get('success'))
+                            <div class="alert alert-primary" role="alert">
+                                {{ $mensaje }}
+                            </div>
+                        @endif
+                        <table class="table table-sm table-bordered table-hover" style="text-align: center;">
+                            <thead class="center">
+                                <th>ID</th>
+                                <th>Género</th>
+                                <th>Acciones</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($datos as $item)
+                                    <tr>
+                                        <td>{{ $item->id }}</td>
+                                        <td>{{ $item->genero }}</td>
+                                        <td>
+                                            <form action="{{ route('generos.edit', $item->id) }}" method="GET"
+                                                style="display: inline;">
+                                                <button class="btn btn-warning btn-xs">Editar</button>
+                                            </form>
+                                            <button class="btn btn-danger btn-xs" data-bs-toggle="modal"
+                                                data-bs-target="#exampleModal">Eliminar</button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
 
-<div class="card">
-    <h5 class="card-header">Géneros</h5>
-    <div class="card-body">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col col-sm-12">
-                    @if ($mensaje = Session::get('success'))
-                    <div class="alert alert-primary" role="alert">
-                        {{$mensaje}}
-                    </div> 
-                    @endif
-                    <table class="table table-sm table-bordered table-hover" style="text-align: center;">
-                        <thead class="center">
-                            <th>ID</th>
-                            <th>Género</th>
-                            <th>Acciones</th>                            
-                        </thead>
-                        <tbody>
-                            @foreach ($datos as $item)
-                                <tr>
-                                    <td>{{$item->id}}</td>
-                                    <td>{{$item->genero}}</td>                                    
-                                    <td>                                        
-                                        <form action="{{route("generos.edit", $item->id)}}" method="GET" style="display: inline;">
-                                            <button class="btn btn-warning btn-xs">Editar</button>
-                                        </form>                                        
-                                        <button class="btn btn-danger btn-xs" data-bs-toggle="modal" data-bs-target="#exampleModal">Eliminar</button>                                        
-                                    </td>                                   
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-
-                    <a href="{{route("generos.create")}}" class="btn btn-primary">Agregar género</a>
+                        <a href="{{ route('generos.create') }}" class="btn btn-primary">Agregar género</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <!-- Modal -->
+    {{-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
@@ -62,6 +63,5 @@
         </div>
       </div>
     </div>
-  </div>
-
+  </div> --}}
 @endsection
