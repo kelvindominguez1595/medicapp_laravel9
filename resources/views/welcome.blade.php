b89832a576576fb2445322f00970e34ac0110fb2
judithmozo <judithcruz50571@gmail.com>
2023-06-01 19:15:20 -0600

    logoappmedic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,11 @@
 @section('content')
     <div class="mt-5 row container">
         <div class="col-4">
-            <h1 class="fs-1">Bienvenido</h1>
+            <h1 class="fs-1">¡Bienvenidos!</h1>
+            <p><H4>TU MEJOR OPCIÓN EN SALUD</H4></p>
         </div>
         <div class="col-8">
-            <img src="{{ asset('image/enter.svg') }}" class="img img-fluid" alt="">
+            <img src="{{ asset('image/logoAPPMEDIC.png') }}" class="img img-fluid" alt="">
         </div>
     </div>
 @endsection
